f00f6ba461e9fbc234ec4ec17c1a3a2603ab7aad
PramodKumarkn01 <ppramodakn@gmail.com>
2024-04-03 15:51:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const UserR=require('./Routes/UserR')
+const Signinroutes=require('./Routes/Signinroutes')
+const AddTask=require('./Routes/AddTask')
+const TaskGroup=require('./Routes/Tasks')
+const TGroupR = require('./Routes/TGroupR')
+const ForgetPassword =require('./Routes/Forgotpassword')
+const ResetPassword = require('./Routes/Resetpassword')
+const app = express();
+const PORT=5000;
+mongoose.connect('mongodb+srv://Promise:Promise@cluster0.iufeasi.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> console.log("connected successfuly"))
+.catch(err=> console.error("field connection",err));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true}))
+app.use('/api',UserR);
+app.use('/api',Signinroutes);
+app.use('/api',AddTask);
+app.use('/api',TaskGroup);
+app.use('/api',TGroupR);
+app.use('/api',ForgetPassword);
+app.use('/api',ResetPassword);
+
+app.listen(PORT,()=>{
+    console.log(`server is runinng on http://localhost:${PORT}`)
+})  
+module.exports=app;
