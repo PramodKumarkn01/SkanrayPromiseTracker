4f6ae169f46cccb299001e224f30263913bf5335
PramodKumarkn01 <ppramodakn@gmail.com>
2024-06-11 13:47:22 +0530

    nnn

---
+++ modules/TaskSchema.js
@@ -0,0 +1,33 @@
+
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  owner: { id: String, name: String, profilePic: String },
+  // taskGroup:{ id: String, name: String },
+  taskGroup: {
+    groupName: { type: String },
+    groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'TaskGroup' }
+  },
+
+  taskName: { type: String },
+  description: { type: String },
+  audioFile: { type: String },
+  pdfFile: { type: String },
+  people: [{ userId: String, name: String }],
+  startDate: { type: String },
+  endDate: { type: String },
+  reminder: { type: String },
+  status: { type: String },
+  category: { type: String },
+  comment: { type: String },
+  remark: {
+    text: { type: String, },
+    date: { type: String, }
+  },
+  createdAt: { type: Date }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
+
